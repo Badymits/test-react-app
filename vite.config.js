39cb110c32b0375889147cee5e4d6bbab3b8b41b
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-09 16:27:27 +0800

    updated config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',  // Set the root directory
+  build: {
+    outDir: 'dist', // Set the build output directory
+  },
 })
